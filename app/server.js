9ba31e5ccec6326198c9ef1cb9a591d3a8cad91f
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-29 19:01:43 +0330

    change config server

--- app/server.js
+++ app/server.js
@@ -53,7 +53,7 @@ class Application {
   }
 
   allowCrossDomain = (req, res, next) => {
-    res.header(`Access-Control-Allow-Origin`, `https://api.hormozganfile.info`);
+    res.header(`Access-Control-Allow-Origin`, `*`);
     res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
     res.header(`Access-Control-Allow-Headers`, `Content-Type`);
     next();
