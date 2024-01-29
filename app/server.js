d58bf757ce6cba655e42558c377c2f7477953b3f
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-29 21:37:37 +0330

    change config server

--- app/server.js
+++ app/server.js
@@ -38,16 +38,35 @@ class Application {
       .catch((err) => console.log('Failed to connect to MongoDB', err));
   }
 
-  allowCrossDomain = (req, res, next) => {
-    res.header(`Access-Control-Allow-Origin`, '*');
-    // res.header(`Access-Control-Allow-Origin`, `http://localhost:3000`);
-    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-    next();
-  };
+  // allowCrossDomain = (req, res, next) => {
+  //   res.header(`Access-Control-Allow-Origin`, '*');
+  //   // res.header(`Access-Control-Allow-Origin`, `http://localhost:3000`);
+  //   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+  //   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+  //   next();
+  // };
   configServer() {
-    this.app.use(this.allowCrossDomain);
-    this.app.use(cors());
+    app.use((req, res, next) => {
+      res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://hormozganfile.info'
+      );
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
+      );
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
+      );
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      res.setHeader('Access-Control-Allow-Private-Network', true);
+      //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+      res.setHeader('Access-Control-Max-Age', 7200);
+
+      next();
+    });
+    // this.app.use(cors());
     // this.app.use(
     //   cors({ credentials: true, origin: process.env.ALLOW_CORS_ORIGIN })
     // );
