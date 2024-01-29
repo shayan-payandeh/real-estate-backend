c888b6e33d15cdd766e7025b95e323cbf32a53d8
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-29 22:17:05 +0330

    change config server

--- app/server.js
+++ app/server.js
@@ -47,23 +47,26 @@ class Application {
   // };
   configServer() {
     app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'https://hormozganfile.info');
-      res.header(
+      res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://hormozganfile.info'
+      );
+      res.setHeader(
         'Access-Control-Allow-Methods',
         'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
       );
-      res.header(
+      res.setHeader(
         'Access-Control-Allow-Headers',
         'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
       );
-      res.header('Access-Control-Allow-Credentials', true);
-      // res.header('Access-Control-Allow-Private-Network', true);
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      res.setHeader('Access-Control-Allow-Private-Network', true);
       //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-      res.header('Access-Control-Max-Age', 7200);
+      res.setHeader('Access-Control-Max-Age', 7200);
 
       next();
     });
-    this.app.use(cors());
+    // this.app.use(cors());
     // this.app.use(
     //   cors({ credentials: true, origin: process.env.ALLOW_CORS_ORIGIN })
     // );
