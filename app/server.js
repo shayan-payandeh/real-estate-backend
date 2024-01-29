fc56cb18e3b89b9dcf05d10e180b66f8faf1f01e
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-30 00:06:03 +0330

    change config server

--- app/server.js
+++ app/server.js
@@ -46,31 +46,27 @@ class Application {
   //   next();
   // };
   configServer() {
-    this.app.use((req, res, next) => {
-      res.setHeader(
-        'Access-Control-Allow-Origin',
-        'https://hormozganfile.info'
-      );
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE'
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
-      );
-      res.header('Access-Control-Allow-Credentials', true);
-      res.header('Access-Control-Allow-Private-Network', true);
-      //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-      res.setHeader('Access-Control-Max-Age', 7200);
-
-      next();
-    });
-    this.app.use(cors());
-
     this.app.use(
-      cors({ credentials: true, origin: 'https://hormozganfile.info' })
+      cors({
+        origin: 'https://hormozganfile.info', // use your actual domain name (or localhost), using * is not recommended
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+        allowedHeaders: [
+          'Content-Type',
+          'Origin',
+          'X-Requested-With',
+          'Accept',
+          'x-client-key',
+          'x-client-token',
+          'x-client-secret',
+          'Authorization',
+        ],
+        credentials: true,
+      })
     );
+
+    // this.app.use(
+    //   cors({ credentials: true, origin: 'https://hormozganfile.info' })
+    // );
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(`/images`, express.static(`app/uploads`));
