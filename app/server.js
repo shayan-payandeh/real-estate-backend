e302acead63891a26cfefbe470deb1fedcafe19c
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-29 23:35:53 +0330

    change config server

--- app/server.js
+++ app/server.js
@@ -61,7 +61,7 @@ class Application {
         'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
       );
       res.setHeader('Access-Control-Allow-Credentials', true);
-      // res.setHeader('Access-Control-Allow-Private-Network', true);
+      res.setHeader('Access-Control-Allow-Private-Network', true);
       //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
       // res.setHeader('Access-Control-Max-Age', 7200);
 
